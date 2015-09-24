17909409b714233dddd9adcd5bdd28e4860a2531
Brozerker <chris.randall619@yahoo.com>
2015-09-24 14:45:49 -0700

    sprite loading

--- UIMain.h
+++ UIMain.h
@@ -9,33 +9,34 @@
 
 #include "From Main\GameState.h"
 #include "From Sam\GFX.h"
-
-// Sprite order
-#define HELP 0
+#include "defines.h"
+#include <vector>
+using namespace std;
 
 
 class UIMain {
 	GAMESTATE currentState;
 	GFXCore::Graphics gfx;
 	// Stores the position of sprites that LoadSprite loads into an array so that they can be accessed later
-	int * spriteElementPositions; 
+	vector<int> spriteElementPositions; 
+	vector<int> currentSpritesToRender;
 	int currentScore;
 	int missileAmmo;
 public:
 
-	//UIMain(): currentState(STATE_INIT), currentScore(0), missileAmmo(0) {}
-	//~UIMain();
+	UIMain(): currentState(STATE_INIT), currentScore(0), missileAmmo(0) {}
+	~UIMain();
 
 	void init() {
-		//spriteElementPositions[0] = gfx.loadSprite("background.png");
-		//spriteElementPositions[1] = gfx.loadSprite("title.png");
-		//spriteElementPositions[2] = gfx.loadSprite("play.png");
-		//spriteElementPositions[3] = gfx.loadSprite("credits.png");
-		//spriteElementPositions[4] = gfx.loadSprite("quit.png");
-		//spriteElementPositions[5] = gfx.loadSprite("missile.png");
-		//spriteElementPositions[6] = gfx.loadSprite("healthBackground.png");
-		//spriteElementPositions[7] = gfx.loadSprite("healthBar.png");
-		//spriteElementPositions[8] = gfx.loadSprite("back.png");
+		spriteElementPositions[0] = gfx.loadSprite((wchar_t*)"Sprites\Background.png");
+		spriteElementPositions[1] = gfx.loadSprite((wchar_t*)"Sprites\Title.png", D3DXVECTOR3(300, 100, 0.0f));
+		spriteElementPositions[2] = gfx.loadSprite((wchar_t*)"Sprites\Start.png", D3DXVECTOR3(350, 300, 0.0f));
+		spriteElementPositions[3] = gfx.loadSprite((wchar_t*)"Sprites\Credits.png", D3DXVECTOR3(350, 400, 0.0f));
+		spriteElementPositions[4] = gfx.loadSprite((wchar_t*)"Sprites\missile.png");
+		spriteElementPositions[5] = gfx.loadSprite((wchar_t*)"Sprites\healthBackground.png");
+		spriteElementPositions[6] = gfx.loadSprite((wchar_t*)"Sprites\healthBar.png");
+		spriteElementPositions[7] = gfx.loadSprite((wchar_t*)"Sprites\Paused.png", D3DXVECTOR3(300, 100, 0.0f));
+		spriteElementPositions[8] = gfx.loadSprite((wchar_t*)"Sprites\Back.png", D3DXVECTOR3(350, 400, 0.0f));
 	}
 
 	GAMESTATE checkStateChanges() {
@@ -63,21 +64,33 @@ public:
 	}
 
 	void updateMenu() {
-		// tell Graphics which sprites to draw
+		currentSpritesToRender.clear();
+		currentSpritesToRender.push_back(BACKGROUND);
+		currentSpritesToRender.push_back(TITLE);
+		currentSpritesToRender.push_back(START);
+		currentSpritesToRender.push_back(CREDITS);
+		// tell Graphics which sprites to draw (0,1,2,3)
 		// check for mouse input
 		// for each button
 			// was mouse input within button?
 			// change currentState accordingly
 	}
 	void updateGame() {
-		// tell Graphics which sprites to draw
+		currentSpritesToRender.clear();
+		currentSpritesToRender.push_back(BACKGROUND);
+		currentSpritesToRender.push_back(MISSILE);
+		currentSpritesToRender.push_back(HPBACKGROUND);
+		currentSpritesToRender.push_back(HEALTH);
+		// tell Graphics which sprites to draw (0, 4, 5, 6)
 		// update health bar and number of missiles
 		// Main has getHealth(), getMaxHealth(), and getMissileAmmo() functions that we can use. \
 		   They've also provided me with their code. I haven't uploaded it, but if you want to \
 		   see it, let me know.
 	}
 	void updatePause() {
-		// tell Graphics which sprites to draw
+		currentSpritesToRender.push_back(PAUSED);
+		currentSpritesToRender.push_back(BACK);
+		// tell Graphics which sprites to draw (7,8)
 		// check for mouse input
 		// was button pressed?
 			// change currentState accordingly
