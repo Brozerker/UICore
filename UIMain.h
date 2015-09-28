330c9af578008a4a7f82dd74e2b42f86267a0827
WillBowden <william.t.bowden@Gmail.com>
2015-09-27 18:57:47 -0700

    Merging Files to UI Core
    Merged the working code I tested in another program and put it in UI
    Main

--- UIMain.h
+++ UIMain.h
@@ -17,6 +17,25 @@ using namespace std;
 using GFXCore::SpriteData;
 
 
+/* -------- Williams's Additions ----------- */
+#include "From Bill\UISpriteAnimation.h"
+#include "From Bill\libraries.h"
+
+
+typedef list<CSprite> SPRITELIST;
+
+SPRITELIST				SPRITE_LIST;
+SPRITELIST::iterator	SPRITE_A;
+SPRITELIST::iterator	SPRITE_B;
+SPRITELIST::iterator	SPRITE_C;
+
+void    InitializeSprites(void);
+void    ShipScore(void);
+
+int   SCORE_BOARD       = 0;
+int   HEALTH_BAR		= 9;
+/* ---------------------------------------- */
+
 class UIMain {
 	GAMESTATE currentState;
 	GFXCore::Graphics gfx;
@@ -137,3 +156,107 @@ public:
 		currentSpritesToRender(GAMEOVER);
 	}
 };
+
+
+/* -------- Williams's Additions ----------- */
+void InitializeSprites(void)
+{
+    CSprite sprite;
+    int i = 0;
+
+	//RED HEALTH (FIRST)
+    sprite.zeroSpriteValues();
+    wcscpy_s( sprite.m_spriteType, L"redHP" );
+    wcscpy_s( sprite.m_textureName, L"misc.bmp" );
+    sprite.m_spriteW				= 200;
+    sprite.m_spriteH				= 200;
+    sprite.m_isActive				= false;
+    sprite.m_oneFrame				= true;
+    sprite.m_offsetX				= 51;
+    sprite.m_offsetY				= 0;
+    sprite.m_frameW					= 30;
+    sprite.m_frameH					= 40;
+
+    SPRITE_LIST.push_back(sprite);
+
+	//YELLOW HEALTH (SECOND)
+    sprite.zeroSpriteValues();
+    wcscpy_s( sprite.m_spriteType, L"yellowHP" );
+    wcscpy_s( sprite.m_textureName, L"misc.bmp" );
+    sprite.m_spriteW				= 200;
+    sprite.m_spriteH				= 200;
+    sprite.m_isActive				= false;
+    sprite.m_oneFrame				= true;
+    sprite.m_offsetX				= 81;
+    sprite.m_offsetY				= 0;
+    sprite.m_frameW					= 30;
+    sprite.m_frameH					= 40;
+    
+    SPRITE_LIST.push_back(sprite);
+
+	//GREEN HEALTH (THIRD)
+    sprite.zeroSpriteValues();
+    wcscpy_s( sprite.m_spriteType, L"greenHP" );
+    wcscpy_s( sprite.m_textureName, L"misc.bmp" );
+    sprite.m_spriteW				= 200;
+    sprite.m_spriteH				= 200;
+    sprite.m_isActive				= false;
+    sprite.m_oneFrame				= true;
+    sprite.m_offsetX				= 111;
+    sprite.m_offsetY				= 0;
+    sprite.m_frameW					= 30;
+    sprite.m_frameH					= 40;
+
+    SPRITE_LIST.push_back(sprite);
+
+	//SCORE NUMBER FONT
+    for( i = 0; i < 4; i++ )
+    {
+        sprite.zeroSpriteValues();
+        wcscpy_s( sprite.m_spriteType, L"numbers" );
+        wcscpy_s( sprite.m_textureName, L"numbers.bmp" );
+        sprite.m_spriteW             = 185;
+        sprite.m_spriteH             = 27;
+        sprite.m_State			     = i;
+        sprite.m_posX				 = (float)600 + (i * 17);
+        sprite.m_posY				 = 572;
+        sprite.m_frameAnimate        = false;
+        sprite.m_offsetX			 =  9;
+        sprite.m_offsetY			 =  5;
+        sprite.m_frameAcross         = 10;
+        sprite.m_frameW				 = 17;
+        sprite.m_frameH				 = 17;
+        sprite.loadAnimationString( 0, "0 1 2 3 4 5 6 7 8 9", CSprite::LOOP_ANIMATION );
+
+        SPRITE_LIST.push_back(sprite);
+    }
+}
+
+void ShipScore(void)
+{
+
+	int number4 = SCORE_BOARD % 10; 
+    int number3 = (SCORE_BOARD % 100)/10;
+    int number2 = (SCORE_BOARD % 1000)/100;
+    int number1 = (SCORE_BOARD % 10000)/1000;
+
+    for( SPRITE_A = SPRITE_LIST.begin(); SPRITE_A != SPRITE_LIST.end(); ++SPRITE_A )
+    {
+        if( !lstrcmp(SPRITE_A->m_spriteType, L"numbers") )
+        {
+            if( SPRITE_A->m_State == 0 )
+                SPRITE_A->m_CurrFrame = number1;
+
+            if( SPRITE_A->m_State == 1 )
+                SPRITE_A->m_CurrFrame = number2;
+
+            if( SPRITE_A->m_State == 2 )
+                SPRITE_A->m_CurrFrame = number3;
+
+            if( SPRITE_A->m_State == 3 )
+                SPRITE_A->m_CurrFrame = number4;
+        }
+
+    }
+}
+/* ---------------------------------------- */
