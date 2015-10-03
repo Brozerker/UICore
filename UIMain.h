fb5d30f9dc62e3126470faf1b71a88fd2c8fc31b
WillBowden <william.t.bowden@Gmail.com>
2015-10-03 16:10:52 -0700

    Update health, missiles, score

--- UIMain.h
+++ UIMain.h
@@ -15,25 +15,13 @@
 using namespace std;
 using GFXCore::SpriteData;
 
+static int		currHealth		= HEALTH;
+static int		currAmmo		= MISSILE;
+static int		currPoint		= 0;
 
-/* -------- Williams's Additions ----------- */
-#include "From Bill\UISpriteAnimation.h"
-#include "From Bill\libraries.h"
+float g_posX;
+float g_posY;
 
-
-typedef list<CSprite> SPRITELIST;
-
-SPRITELIST				SPRITE_LIST;
-SPRITELIST::iterator	SPRITE_A;
-SPRITELIST::iterator	SPRITE_B;
-SPRITELIST::iterator	SPRITE_C;
-
-void    InitializeSprites(void);
-void    ShipScore(void);
-
-int   SCORE_BOARD       = 0;
-int   HEALTH_BAR		= 9;
-/* ---------------------------------------- */
 
 class UIMain {
 protected:
@@ -86,14 +74,55 @@ public:
 			}
 		}
 	}
+
+	void updateScore()
+	{
+	
+		wchar_t scoreTxt[256];
+		swprintf(scoreTxt, 256, L"Point(s): %i", currPoint);
+
+	}
+
 	void updateGame() {
+		float healthPosX = 300;
+		float healthPosY = 100;
+
+		float ammoPosX = 200;
+		float ammoPosY = 100;
+
+		
+
 		// tells graphics what to draw
 		GFX->addToSpriteRenderList((int*)gameSpriteIDs[0], gameSpriteIDs.size());
 
 		// update health bar and number of missiles
+
+		//Health bar update
+		g_posX = healthPosX;
+			if(HEALTH > currHealth)
+			{
+				for(int i = 0; i < currHealth; ++i)
+				{
+					GFX->updateSprite((wchar_t)"hp", D3DXVECTOR3(g_posX, healthPosY, 0.0f)); //update the next health bar node position
+
+					//add printsprite() function to display the health
+					
+					g_posX + 10; // increase the distance from the previous health bar node to the next node.
+				}
+				g_posX = healthPosX; // reset x coord position
+			}
+		
+		//Missle Bar Update
+		GFX->updateSprite((wchar_t)"missles", D3DXVECTOR3( ammoPosX, ammoPosY, 0.0f ));
+
+		//add printsprite() function to display the current ammo
+
+
+
 		// Main has getHealth(), getMaxHealth(), and getMissileAmmo() functions that we can use. \
 		   They've also provided me with their code. I haven't uploaded it, but if you want to \
 		   see it, let me know.
+		
 	}
 	void updatePause(const bool paused) {
 		// tells graphics what to draw
@@ -113,108 +142,6 @@ public:
 		GFX->addToSpriteRenderList((int*)exitSpriteIDs[0], exitSpriteIDs.size());
 		currentState = STATE_EXIT;
 	}
+
+
 };
-
-
-/* -------- Williams's Additions ----------- */
-void InitializeSprites(void)
-{
-    CSprite sprite;
-    int i = 0;
-
-	//RED HEALTH (FIRST)
-    sprite.zeroSpriteValues();
-    wcscpy_s( sprite.m_spriteType, L"redHP" );
-    wcscpy_s( sprite.m_textureName, L"misc.bmp" );
-    sprite.m_spriteW				= 200;
-    sprite.m_spriteH				= 200;
-    sprite.m_isActive				= false;
-    sprite.m_oneFrame				= true;
-    sprite.m_offsetX				= 51;
-    sprite.m_offsetY				= 0;
-    sprite.m_frameW					= 30;
-    sprite.m_frameH					= 40;
-
-    SPRITE_LIST.push_back(sprite);
-
-	//YELLOW HEALTH (SECOND)
-    sprite.zeroSpriteValues();
-    wcscpy_s( sprite.m_spriteType, L"yellowHP" );
-    wcscpy_s( sprite.m_textureName, L"misc.bmp" );
-    sprite.m_spriteW				= 200;
-    sprite.m_spriteH				= 200;
-    sprite.m_isActive				= false;
-    sprite.m_oneFrame				= true;
-    sprite.m_offsetX				= 81;
-    sprite.m_offsetY				= 0;
-    sprite.m_frameW					= 30;
-    sprite.m_frameH					= 40;
-    
-    SPRITE_LIST.push_back(sprite);
-
-	//GREEN HEALTH (THIRD)
-    sprite.zeroSpriteValues();
-    wcscpy_s( sprite.m_spriteType, L"greenHP" );
-    wcscpy_s( sprite.m_textureName, L"misc.bmp" );
-    sprite.m_spriteW				= 200;
-    sprite.m_spriteH				= 200;
-    sprite.m_isActive				= false;
-    sprite.m_oneFrame				= true;
-    sprite.m_offsetX				= 111;
-    sprite.m_offsetY				= 0;
-    sprite.m_frameW					= 30;
-    sprite.m_frameH					= 40;
-
-    SPRITE_LIST.push_back(sprite);
-
-	//SCORE NUMBER FONT
-    for( i = 0; i < 4; i++ )
-    {
-        sprite.zeroSpriteValues();
-        wcscpy_s( sprite.m_spriteType, L"numbers" );
-        wcscpy_s( sprite.m_textureName, L"numbers.bmp" );
-        sprite.m_spriteW             = 185;
-        sprite.m_spriteH             = 27;
-        sprite.m_State			     = i;
-        sprite.m_posX				 = (float)600 + (i * 17);
-        sprite.m_posY				 = 572;
-        sprite.m_frameAnimate        = false;
-        sprite.m_offsetX			 =  9;
-        sprite.m_offsetY			 =  5;
-        sprite.m_frameAcross         = 10;
-        sprite.m_frameW				 = 17;
-        sprite.m_frameH				 = 17;
-        sprite.loadAnimationString( 0, "0 1 2 3 4 5 6 7 8 9", CSprite::LOOP_ANIMATION );
-
-        SPRITE_LIST.push_back(sprite);
-    }
-}
-
-void ShipScore(void)
-{
-
-	int number4 = SCORE_BOARD % 10; 
-    int number3 = (SCORE_BOARD % 100)/10;
-    int number2 = (SCORE_BOARD % 1000)/100;
-    int number1 = (SCORE_BOARD % 10000)/1000;
-
-    for( SPRITE_A = SPRITE_LIST.begin(); SPRITE_A != SPRITE_LIST.end(); ++SPRITE_A )
-    {
-        if( !lstrcmp(SPRITE_A->m_spriteType, L"numbers") )
-        {
-            if( SPRITE_A->m_State == 0 )
-                SPRITE_A->m_CurrFrame = number1;
-
-            if( SPRITE_A->m_State == 1 )
-                SPRITE_A->m_CurrFrame = number2;
-
-            if( SPRITE_A->m_State == 2 )
-                SPRITE_A->m_CurrFrame = number3;
-
-            if( SPRITE_A->m_State == 3 )
-                SPRITE_A->m_CurrFrame = number4;
-        }
-
-    }
-}
-/* ---------------------------------------- */
